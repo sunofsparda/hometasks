d17fbd6bb47d07b698c5f67670ec26803b9bfa11
Anton Cherlyonok <sunofsparda@gmail.com>
2017-03-14 17:35:41 +0300

    task3

---
+++ hometask3.py
@@ -0,0 +1,20 @@
+# Task 1
+# https://www.codewars.com/kata/polynomials-i-string-format
+
+
+# Task 2
+# https://www.codewars.com/kata/sum-of-numbers-from-0-to-n
+
+
+# Task 3
+# https://www.codewars.com/kata/reverser
+
+
+# Task 4
+# https://www.codewars.com/kata/plotting-points-on-a-grid
+
+
+# Task 5
+# https://www.codewars.com/kata/a-disguised-sequence-i
+
+
