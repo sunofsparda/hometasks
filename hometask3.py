9e26702c106576cee6b1710d4b15974359a07d8c
Sun <sunofsparda@gmail.com>
2017-03-14 23:11:07 +0300

    task3_2,3

--- hometask3.py
+++ hometask3.py
@@ -4,11 +4,26 @@
 
 # Task 2
 # https://www.codewars.com/kata/sum-of-numbers-from-0-to-n
+def show_sequence(n):
+    str = "0"
+    if n == 0:
+        result = "0=0"
+    if n < 0:
+        result = "str(n)<0"
+    if n > 0:
+        i +=1
+        str = str + "+" + str(i)
+        result = str + " = " + str
+    return result
+
 
 
 # Task 3
 # https://www.codewars.com/kata/reverser
-
+def reverse(n):
+    """Returns n with all digits reversed. Assume positive n."""
+    i = str(n)
+    return int(i[::-1])
 
 # Task 4
 # https://www.codewars.com/kata/plotting-points-on-a-grid
