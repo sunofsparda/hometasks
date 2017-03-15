b8f4fab13e424e7c2d3b0843b0d68d2ab25d2ca3
Anton Cherlyonok <sunofsparda@gmail.com>
2017-03-15 16:11:43 +0300

    task4

---
+++ hometask4.py
@@ -0,0 +1,15 @@
+# Task1
+# https://www.codewars.com/kata/fractions-class/train/python
+
+
+# Task 2
+# https://www.codewars.com/kata/professor-oaks-trouble-new-pokedex-prototype
+
+
+# Task 3
+# https://www.codewars.com/kata/anything
+
+
+# Task 4
+# https://www.codewars.com/kata/shop-inventory-manager (Optional)
+
